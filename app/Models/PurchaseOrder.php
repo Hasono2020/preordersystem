27a965112f3dcae93cf22a40dc93f8a2b0128d7c
Hasono2020 <hasono.hasono@gmail.com>
2026-05-30 22:22:27 +0700

    fix error purchase controller and model

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Trip;
+use App\Models\User;
 
 class PurchaseOrder extends Model
 {
